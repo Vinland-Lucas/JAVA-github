55093ad3f5a43936d2dadbcf383881ea4eff69ed
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-08-29 17:02:30 +0200

    ADD MicroServices WebApp PROJECT

---
+++ INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/main/java/com/webapp/microserviceswebapp/MicroserviceswebappApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.microserviceswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceswebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceswebappApplication.class, args);
+	}
+
+}
---
+++ INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/test/java/com/webapp/microserviceswebapp/MicroserviceswebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.microserviceswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceswebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
