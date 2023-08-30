aa7e36ac7a4064b9767f6e7824d221124f858683
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-08-30 16:59:32 +0200

    ADD DAO for MicroServices WebApp PROJECT

---
+++ INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/main/java/com/webapp/microserviceswebapp/web/dao/CustomerDao.java
@@ -0,0 +1,11 @@
+package com.webapp.microserviceswebapp.web.dao;
+
+import com.webapp.microserviceswebapp.model.Customer;
+
+import java.util.List;
+
+public interface CustomerDao {
+    List<Customer> findAll();
+    Customer findById(int id);
+    Customer save(Customer customer);
+}
---
+++ INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/main/java/com/webapp/microserviceswebapp/web/dao/CustomerDaoImpl.java
@@ -0,0 +1,41 @@
+package com.webapp.microserviceswebapp.web.dao;
+
+import com.webapp.microserviceswebapp.model.Customer;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao {
+    public static List<Customer> customers = new ArrayList<>();
+
+    static {
+        customers.add(new Customer(1, "Kageyama", "Tobio", new Date(), "Y6JF83SE7"));
+        customers.add(new Customer(2, "Hinata", "Shoyo", new Date(), "FYGFFE92Z"));
+        customers.add(new Customer(3, "Miya", "Atsumu", new Date(), "Z5E62TBN4"));
+        customers.add(new Customer(4, "Hoshiumi", "Koray", new Date(), "D03JKZ81A"));
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customers;
+    }
+
+    @Override
+    public Customer findById(int id) {
+        for (Customer customer : customers) { // Pour chaque(for-each) élément "Customer customer" dans la liste "customers"
+           if (customer.getId() == id) { // Si l'id du client (customer.getId) est égal à l'id recherché(id)...
+               return customer;          // ... alors renvoie ce client et cela interrompt la boucle
+           }
+        }
+        return null; // renvoie "null" s'il n'y a pas d'id du client correspondant à l'id recherché
+    }
+
+    @Override
+    public Customer save(Customer customer) { // méthode permettant d'ajouter un client à notre liste de client "customers"
+        customers.add(customer); // customer --> Customer customer = new Customer()
+        return customer;
+    }
+}
