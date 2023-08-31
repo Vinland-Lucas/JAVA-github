46b465d819b7a8ca421a8e4f9e6bbe4bec36cb2d
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-08-31 15:14:01 +0200

    ADD POST REQUEST to add new customer

--- INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/main/java/com/webapp/microserviceswebapp/web/controller/CustomerController.java
+++ INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/main/java/com/webapp/microserviceswebapp/web/controller/CustomerController.java
@@ -2,35 +2,35 @@ package com.webapp.microserviceswebapp.web.controller;
 
 
 import com.webapp.microserviceswebapp.model.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.webapp.microserviceswebapp.web.dao.CustomerDao;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CustomerController {
 
-    ArrayList<Customer> customers = new ArrayList<Customer>(
-            List.of(
-                    new Customer(1, "Kageyama", "Tobio", new Date(), "Y6JF83SE7"),
-                    new Customer(2, "Hinata", "Shoyo", new Date(), "FYGFFE92Z"),
-                    new Customer(3, "Miya", "Atsumu", new Date(), "Z5E62TBN4"),
-                    new Customer(4, "Hoshiumi", "Koray", new Date(), "D03JKZ81A")
-            )
-    );
+    private final CustomerDao customerDao;
+
+    public CustomerController(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
 
     @GetMapping("/customers")
-    public ArrayList listeClients() {
-        return customers;
+    public List<Customer> listeClients() {
+        return customerDao.findAll();
     }
 
     @GetMapping("/customers/{id}")
     public Customer afficherUnClient(@PathVariable int id) {
-        Customer customer = new Customer(id, "Kageyama", "Tobio", new Date(), "Y6JF83SE7");
-        return customer;
+        return customerDao.findById(id);
     }
+
+    @PostMapping("/customers")
+    public void ajouterUnClient(@RequestBody Customer customer) {
+        customerDao.save(customer);
+    }
+
 
 }
