c73e855c151b13bc16a987b4b293130497b307bc
Vinland-Lucas <lucas.hoang@le-campus-numerique.fr>
2023-09-04 02:41:55 +0200

    ADD REST API CALL with RESTTEMPLATE to consume an API (END OF ITERATION 3)

--- INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/main/java/com/webapp/microserviceswebapp/web/controller/CustomerController.java
+++ INTRO_JAVA_SPRINGBOOT/webapp/microserviceswebapp/src/main/java/com/webapp/microserviceswebapp/web/controller/CustomerController.java
@@ -5,7 +5,10 @@ import com.webapp.microserviceswebapp.model.Customer;
 import com.webapp.microserviceswebapp.web.dao.CustomerDao;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 import java.util.List;
@@ -15,10 +18,23 @@ import java.util.List;
 @RestController
 public class CustomerController {
 
+    RestTemplate restTemplate = new RestTemplate();
+
     private final CustomerDao customerDao;
+    private final String url = "http://localhost:8081/licenses/";
 
     public CustomerController(CustomerDao customerDao) {
         this.customerDao = customerDao;
+    }
+
+    public void validDriverLicense(String driverLicenseNumber) {
+        Boolean isValid = restTemplate.getForObject(url + driverLicenseNumber, Boolean.class);
+
+        if (isValid == false) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Invalid Driving License"
+            );
+        }
     }
 
     @ApiOperation(value = "méthode permettant d'afficher tous les clients de notre liste ou BDD à travers une requête de type GET. Cette méthode ne répondra qu'à une requête ayant une URL de type /customers")
@@ -36,12 +52,14 @@ public class CustomerController {
     @ApiOperation(value = "méthode permettant d'ajouter un client à notre liste ou BDD à travers une requête de type POST. Cette méthode ne répondra qu'à une requête ayant une URL de type /customers")
     @PostMapping("/customers")
     public void ajouterUnClient(@RequestBody Customer customer) {
+        validDriverLicense(customer.getDriverLicenseNumber());
         customerDao.save(customer);
     }
 
     @ApiOperation(value = "méthode permettant d'update un client dans notre liste ou BDD en utilisant une requête PUT. Cette méthode ne répondra qu'à une requête ayant une URL de type customers/id")
     @PutMapping("/customers/{id}")
     public void modifierUnClient(@PathVariable int id, @RequestBody Customer customer) {
+        validDriverLicense(customer.getDriverLicenseNumber());
         customerDao.update(id, customer);
     }
 
